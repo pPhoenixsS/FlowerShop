b7c4bb64ae6ac5265bfb0e438f352968631c3919
ddovwll <robertinkashenoid@gmail.com>
2024-05-18 19:21:36 +0300

    добавлены модели
    модель товара и базы данных

---
+++ Goods/DAL/DbHelper.cs
@@ -0,0 +1,14 @@
+﻿using Identity.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.DAL;
+
+public class DbHelper : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=4321;Database=GoodsDb;Username=User;Password=qwe123asd");
+    }
+    
+    public DbSet<Product> Products { get; set; }
+}
