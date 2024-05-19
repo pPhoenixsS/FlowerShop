a0c04fefae58ec531a5a8430f3d548f8598a0aaa
ddovwll <robertinkashenoid@gmail.com>
2024-05-19 16:13:04 +0300

    наброски логики для продукта

---
+++ Goods/BLL/ProductBll.cs
@@ -0,0 +1,35 @@
+﻿using Goods.DAL;
+using Goods.DAL.Models;
+using Goods.Exceptions;
+
+namespace Goods.BLL;
+
+public class ProductBll(IProductDal productDal) : IProductBll
+{
+    public async Task<Product> CreateProductAsync(Product product)
+    {
+        await productDal.CreateProductAsync(product);
+        return product;
+    }
+
+    public async Task<Product> GetProductAsyncById(int id)
+    {
+        var productFromDb = await productDal.GetProductAsyncById(id);
+        if (productFromDb.Id == 0)
+            throw new NotFoundException();
+        return productFromDb;
+    }
+
+    public async Task<Product> UpdateProductAsync(Product product)
+    {
+        await productDal.UpdateProductAsync(product);
+        return product;
+    }
+
+    public async Task DeleteProductAsync(int id)
+    {
+        if ((await productDal.GetProductAsyncById(id)).Id == 0)
+            throw new NotFoundException();
+        await productDal.DeleteProductAsync(id);
+    }
+}
--- Goods/Program.cs
+++ Goods/Program.cs
@@ -1,3 +1,6 @@
+using Goods.BLL;
+using Goods.DAL;
+
 namespace Goods;
 
 public class Program
@@ -12,6 +15,9 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IProductDal, ProductDal>();
+        builder.Services.AddScoped<IProductBll, ProductBll>();
 
         var app = builder.Build();
 
